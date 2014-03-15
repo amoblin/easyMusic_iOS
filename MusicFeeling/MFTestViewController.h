d97be6974d53a75b206078d679468771833c0f85
amoblin@mpb <amoblin@gmail.com>
2014-03-15 11:51:05 +0800

    add test page

---
+++ MusicFeeling/MFTestViewController.h
@@ -0,0 +1,15 @@
+//
+//  MFTestViewController.h
+//  MusicFeeling
+//
+//  Created by amoblin on 14-3-15.
+//  Copyright (c) 2014年 amoblin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MFTestViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *replayButton;
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+@end
