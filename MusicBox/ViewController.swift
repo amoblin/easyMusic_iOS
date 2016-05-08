581b7591f51d88d2c1fbdcdd07a476df02b5d635
amoblin@mbp <amoblin@gmail.com>
2016-05-08 21:26:08 +0800

    new target: MusicBox

---
+++ MusicBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicBox
+//
+//  Created by amoblin on 16/5/8.
+//  Copyright © 2016年 amoblin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
